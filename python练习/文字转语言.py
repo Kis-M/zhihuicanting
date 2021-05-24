b1fd276bcc0e86a5880343da6e56b6f4fe6b5393
mahaoyong <1902006704@qq.com>
2021-05-24 11:36:34 +0800

    5.24

--- python练习/文字转语言.py
+++
@@ -1,7 +0,0 @@
-import pyttsx3
-
-
-if __name__ == '__main__':
-    engine = pyttsx3.init()
-    engine.say("你好你好你好")
-    engine.runAndWait()
---
+++ test/test.py
@@ -0,0 +1,4 @@
+import random
+
+randomnum = [random.randint(10, 100) for i in range(10)]
+print(randomnum)
